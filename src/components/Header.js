7206fdfe0a670882ce2360b91ab7be23587059d4
raza <101937798+razaoffline@users.noreply.github.com>
2022-04-18 14:55:23 -0400

    upload src/components/Header

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFileCode } from '@fortawesome/free-solid-svg-icons';
+
+
+const Header = (props) => {
+    return (
+        <Navbar bg="light" fixed="top">
+            <Navbar.Text className="Header" style={{ color: "black", verticalAlign: "middle" }}>
+                <strong>Fastrdle</strong>
+            </Navbar.Text>
+            <Navbar.Text className="Repository">
+                <a href="https://github.com/">
+                    <FontAwesomeIcon icon={faFileCode} size="lg" />
+                </a>
+            </Navbar.Text>
+        </Navbar>
+    )
+}
+
+export default Header;
